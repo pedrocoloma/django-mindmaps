39170ebb58203a5601625c65b003c82d0d3358ec
Pedro Coloma <pedrocolomaa@gmail.com>
2018-09-02 15:54:14 -0300

    Added User and Map models

--- mindmaps/models.py
+++ mindmaps/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+    language = models.CharField(max_length=8)
+    nickname = models.CharField(max_length=32)
+    public_id = models.CharField(max_length=16)
+    bio = models.TextField()
+    # picture
+    # maps has many
+    # creation date
+    # validated email
+
+class Map(models.Model):
+    title = models.CharField(max_length=128)
+    ispublic = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="mymaps")
+    # creation timestamp
+    public_id = models.CharField(max_length=16)
+    friendly_url = models.CharField(max_length=128)
+    language = models.CharField(max_length=8)
+    # keywords
+    # views
+    # number of shares
+
+"""
+Map
+    id, title, public, author, timestamp, public_id, friemdly_url, language, keywords
+User
+    id, public_id, name, email, password, language, nickname, bio, picture, maps
+Reads
+    id, user_id, was_user_registered, timestamp
+Trends
+Featured
+"""
--- mindmaps/views.py
+++ mindmaps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Map
 
 # Create your views here.
+def index(request):
+    context = {
+        "maps": Map.objects.all()
+    }
+    return render(request, "maps/index.html")
