ab705f6d4ddb493d0b242b912f2519dc6d7720ad
Pedro Coloma <pedrocolomaa@gmail.com>
2018-09-09 16:53:08 -0300

    Added New MindMap and MyMindMaps

--- mindmaps/urls.py
+++ mindmaps/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('m/new/', views.newmindmap, name="newmindmap"),
     path("m/<int:map_id>", views.map, name="map"),
     path('u/signup/', views.signup.as_view(), name='signup'),
 ]
--- mindmaps/views.py
+++ mindmaps/views.py
@@ -8,10 +8,20 @@ from .forms import CustomUserCreationForm
 
 # Create your views here.
 def index(request):
-    context = {
-        "maps": Map.objects.all(),
-    }
-    return render(request, "maps/index.html", context)
+    if request.user.is_authenticated:
+        try:
+            maps = Map.objects.filter(author=request.user)
+        except Map.DoesNotExist:
+            raise Http404("No map found")
+        context = {
+            "maps": maps
+        }
+        return render(request, 'maps/mymaps.html', context)
+    else:
+        context = {
+            "maps": Map.objects.all(),
+        }
+        return render(request, "maps/index.html", context)
 
 def map(request, map_id):
     try:
@@ -27,3 +37,22 @@ class signup(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def newmindmap(request):
+    if request.user.is_authenticated:
+        print("Está autenticado")
+    else:
+        print("Não está autenticado")
+
+    if request.method == 'POST':
+        title=request.POST.get('title')
+        ispublic = False
+        author = request.user
+        public_id = ""
+        friendly_url = ""
+        language = "pt-br"
+        Map.objects.create(title=title, ispublic=ispublic, author=author, public_id=public_id, friendly_url=friendly_url, language=language)
+    else:
+        print("Foi GET")
+    context = {}
+    return render(request, 'maps/new.html', context)
