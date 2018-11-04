206f478a042bccd9f2c8314cce0e9d94d9e2697d
Pedro Coloma <pedrocolomaa@gmail.com>
2018-11-04 18:00:06 -0200

    Removed profile

--- mindmaps/urls.py
+++ mindmaps/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     path('u/signup/', views.signup.as_view(), name='signup'),
     path("l/", views.alllistings, name="alllistings"),
     path("l/<int:listing_id>", views.listing, name="listing"),
-    path("p/", views.myprofile, name="myprofile"),
-    path("p/<int:profile_id>", views.publicprofile, name="profile"),
 ]
