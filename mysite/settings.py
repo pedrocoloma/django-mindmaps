dbf6fa26fe6d51043e79632ac3dbdb22b043d8df
Pedro Coloma <pedrocolomaa@gmail.com>
2018-11-04 23:16:48 -0200

    First attempt to fix heroku's buildpack issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,6 @@ AUTH_USER_MODEL = 'mindmaps.CustomUser'
 LOGIN_REDIRECT_URL = 'index'
 
 LOGOUT_REDIRECT_URL = 'index'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
